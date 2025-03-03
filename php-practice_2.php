fb31c1ca9c950c8c060261624eef17d8039a5d64
gaku1130 <gk.99prd3o@gmail.com>
2025-03-03 17:23:53 +0900

    mv: ファイル移動

---
+++ php-practice_2.php
@@ -0,0 +1,24 @@
+<?php
+// Q1 tic-tac問題
+
+foreach($num){
+    if($num % 4 == 0){
+        return tic;
+    }elseif($num % 5 == 0){
+        return tac;
+    }elseif($num % 20 == 0){
+        return tic-tac;
+    }
+}
+
+// Q2 多次元連想配列
+
+
+// Q3 オブジェクト-1
+
+
+// Q4 オブジェクト-2
+
+
+// Q5 定義済みクラス
+?>
